e8649ebcf32c5f9e186c7ef3c9430ce805d82f2e
camilajullyane <camilaj.silva0@gmail.com>
2024-02-22 15:18:11 -0300

    update

--- wine.py
+++ wine.py
@@ -1,16 +1,40 @@
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from sklearn import tree
+# from sklearn.tree import DecisionTreeClassifier
+
+# df = pd.read_csv("WineQT.csv")
+
+# print(df)
+
+# features = ["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol","Id"]
+# variables = df[features]
+# response = df["quality"]
+# dtree = DecisionTreeClassifier(max_depth=5)
+# dtree = dtree.fit(variables, response)
+# tree.plot_tree(dtree, feature_names=features)
+
+# plt.show()
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
 
 df = pd.read_csv("WineQT.csv")
-
-print(df)
 
 features = ["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol","Id"]
 variables = df[features]
 response = df["quality"]
-dtree = DecisionTreeClassifier()
+
+dtree = DecisionTreeClassifier(max_depth=5)
+
+# Aplicando a validação cruzada de 10-folds
+scores = cross_val_score(dtree, variables, response, cv=10)
+
+print("Scores de cada execução da validação cruzada: ", scores)
+print("Média dos scores: ", scores.mean())
+
 dtree = dtree.fit(variables, response)
 tree.plot_tree(dtree, feature_names=features)
 
