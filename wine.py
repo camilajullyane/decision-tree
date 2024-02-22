0f505ebe21861e7abc2a7eb77d55c9a37c022ac5
camilajullyane <camilaj.silva0@gmail.com>
2024-02-22 16:10:01 -0300

    update

--- wine.py
+++ wine.py
@@ -1,20 +1,3 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# from sklearn import tree
-# from sklearn.tree import DecisionTreeClassifier
-
-# df = pd.read_csv("WineQT.csv")
-
-# print(df)
-
-# features = ["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol","Id"]
-# variables = df[features]
-# response = df["quality"]
-# dtree = DecisionTreeClassifier(max_depth=5)
-# dtree = dtree.fit(variables, response)
-# tree.plot_tree(dtree, feature_names=features)
-
-# plt.show()
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import tree
